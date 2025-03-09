9c64c945ef399a4c99ba36c7619a14c352a2f7d4
+ <+>
2025-03-09 19:41:42 +0300

    added functions

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+saturdayFun()
+function saturdayFun(){
+    console.log("This saturday I want to have fun and do some chores");
+}
+
+
+
+const mondayWork = function(action = "go to the office") {
+    return `This Monday, I will ${action}.`;
+  };
+  console.log(mondayWork());
+
+  function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+    console.log(saturdayFun()); 
+
+
+
+// Function wrapAdjective
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+
+  const encouragingPromptFunction = wrapAdjective();
+  console.log(encouragingPromptFunction());
